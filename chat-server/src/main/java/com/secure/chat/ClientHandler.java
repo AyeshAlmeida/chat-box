7832c9e46f29b6fc72bff18278233a65c08c49c3
AyeshAlmeida <ayesh9206@gmail.com>
2017-08-21 23:05:30 +0530

    Upda

---
+++ chat-server/src/main/java/com/secure/chat/ClientHandler.java
@@ -0,0 +1,54 @@
+package com.secure.chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Iterator;
+
+/**
+ * Created by ayesh on 8/20/17.
+ */
+public class ClientHandler implements Runnable {
+    private BufferedReader reader;
+    private Socket socket;
+    private Iterator iterator;
+
+    public ClientHandler(Socket socket, Iterator iterator) {
+        try {
+            this.socket = socket;
+            this.iterator = iterator;
+            InputStreamReader inputStreamReader = new InputStreamReader(this.socket.getInputStream());
+            reader = new BufferedReader(inputStreamReader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        System.out.printf("Into checker--->");
+        String message;
+
+        try {
+            while ((message = reader.readLine()) != null) {
+                System.out.println("Read ["+message+"]");
+                informEveryClient(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void informEveryClient(String message){
+        try {
+            while (iterator.hasNext()) {
+                PrintWriter writer = (PrintWriter) iterator.next();
+                writer.println(message);
+                writer.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chat-server/src/main/java/com/secure/chat/SimpleChatServer.java
@@ -0,0 +1,38 @@
+package com.secure.chat;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * Created by ayesh on 8/19/17.
+ */
+public class SimpleChatServer {
+    private ArrayList clientOutputStreams;
+
+    public void go() {
+        clientOutputStreams = new ArrayList();
+        try {
+            ServerSocket serverSocket = new ServerSocket(4242);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(socket.getOutputStream());
+                clientOutputStreams.add(writer);
+
+                Thread checker = new Thread(new ClientHandler(socket, clientOutputStreams.iterator()));
+                checker.start();
+                System.out.println("Got a connection.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SimpleChatServer server = new SimpleChatServer();
+        server.go();
+    }
+}
